c31f43ba4ebf815ade7ed190534b4b4c6b617fa5
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-05-25 23:15:48 +0500

    init new module

---
+++ telegram-bot-service/src/main/java/kz/baltabayev/telegrambotservice/TelegramBotServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.baltabayev.telegrambotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotServiceApplication.class, args);
+    }
+
+}

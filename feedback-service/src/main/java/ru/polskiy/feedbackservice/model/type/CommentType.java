472e015ffcd37892c03a72ea9c02a50dde113790
hikka242 <glepol03@mail.ru>
2024-05-28 16:25:37 +0300

    added feed-back module and table feedback

---
+++ feedback-service/src/main/java/ru/polskiy/feedbackservice/model/type/CommentType.java
@@ -0,0 +1,6 @@
+package ru.polskiy.feedbackservice.model.type;
+
+public enum CommentType {
+    REVIEW,
+    COMPLAINT
+}
---
+++ feedback-service/src/main/java/ru/polskiy/feedbackservice/model/type/Status.java
@@ -0,0 +1,8 @@
+package ru.polskiy.feedbackservice.model.type;
+
+public enum Status {
+    RESOLVED,
+    IN_PROGRESS,
+    NEW,
+    CLOSED
+}

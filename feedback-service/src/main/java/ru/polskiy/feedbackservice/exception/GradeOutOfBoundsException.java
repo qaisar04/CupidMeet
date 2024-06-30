e44e441f702e68e23a95d3bbc1a603ccf5207080
hikka242 <glepol03@mail.ru>
2024-06-30 13:11:02 +0300

    doc in new Exceptions(forgot them)

--- feedback-service/src/main/java/ru/polskiy/feedbackservice/exception/GradeOutOfBoundsException.java
+++ feedback-service/src/main/java/ru/polskiy/feedbackservice/exception/GradeOutOfBoundsException.java
@@ -1,8 +1,17 @@
 package ru.polskiy.feedbackservice.exception;
 
-public class GradeOutOfBoundsException extends RuntimeException{
+/**
+ * Exception thrown when a grade is out of the valid range.
+ * This exception is used to indicate that a provided grade value is not within the expected range of 1 to 5.
+ */
+public class GradeOutOfBoundsException extends RuntimeException {
 
+    /**
+     * Constructs a new GradeOutOfBoundsException with the specified detail message.
+     *
+     * @param message The detail message, which is appended with the range information.
+     */
     public GradeOutOfBoundsException(String message) {
-        super(message+" is out of range between 1 and 5");
+        super(message + " is out of range between 1 and 5");
     }
 }
--- feedback-service/src/main/java/ru/polskiy/feedbackservice/exception/NoSuchFeedbackException.java
+++ feedback-service/src/main/java/ru/polskiy/feedbackservice/exception/NoSuchFeedbackException.java
@@ -1,8 +1,17 @@
 package ru.polskiy.feedbackservice.exception;
 
-public class NoSuchFeedbackException extends RuntimeException{
+/**
+ * Exception thrown when a feedback object is not found in the database.
+ * This exception is used to indicate that the specified feedback object does not exist and cannot be patched.
+ */
+public class NoSuchFeedbackException extends RuntimeException {
 
+    /**
+     * Constructs a new NoSuchFeedbackException with the specified detail message.
+     *
+     * @param message The detail message, which is appended with the information about non-existence in the database.
+     */
     public NoSuchFeedbackException(String message) {
-        super("This object:"+message+ " doesn't exist in database and can't be patched");
+        super("This object:" + message + " doesn't exist in database and can't be patched");
     }
 }

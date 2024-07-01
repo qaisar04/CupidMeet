9b0fd9ae3fea90a2bcfb424093dd337f26549078
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-07-01 22:56:58 +0500

    init

---
+++ user-matching-service/src/main/java/kz/baltabayev/usermatchingservice/UserMatchingServiceApplication.java
@@ -0,0 +1,12 @@
+package kz.baltabayev.usermatchingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMatchingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMatchingServiceApplication.class, args);
+    }
+}

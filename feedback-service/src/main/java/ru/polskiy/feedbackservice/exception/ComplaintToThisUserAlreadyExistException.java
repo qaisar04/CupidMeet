4959237ef941797acc44b23f9ed0b8942f7a1433
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-07-06 12:38:14 +0500

    new service to compose

--- feedback-service/src/main/java/ru/polskiy/feedbackservice/exception/ComplaintToThisUserAlreadyExistException.java
+++ feedback-service/src/main/java/ru/polskiy/feedbackservice/exception/ComplaintToThisUserAlreadyExistException.java
@@ -5,7 +5,7 @@ package ru.polskiy.feedbackservice.exception;
  */
 public class ComplaintToThisUserAlreadyExistException extends RuntimeException {
 
-    public ComplaintToThisUserAlreadyExistException(Long userId,Long toUserId) {
-        super("User with id:%d already sent complaint to user with id:%d".formatted(userId,toUserId));
+    public ComplaintToThisUserAlreadyExistException(Long userId, Long toUserId) {
+        super("User with id: %d already sent complaint to user with id: %d".formatted(userId, toUserId));
     }
 }

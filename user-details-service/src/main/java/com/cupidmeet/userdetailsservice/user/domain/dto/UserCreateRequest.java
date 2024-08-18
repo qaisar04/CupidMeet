1563fea6048303a4e9298c354b8e1da03ab05e15
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-08-18 13:41:22 +0300

    Добавлена валидация данных

--- user-details-service/src/main/java/com/cupidmeet/userdetailsservice/user/domain/dto/UserCreateRequest.java
+++ user-details-service/src/main/java/com/cupidmeet/userdetailsservice/user/domain/dto/UserCreateRequest.java
@@ -1,6 +1,9 @@
 package com.cupidmeet.userdetailsservice.user.domain.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 /**
@@ -13,24 +16,28 @@ public class UserCreateRequest {
     /**
      * Уникальный идентификатор пользователя в Telegram.
      */
+    @NotNull
     @Schema(description = "Уникальный идентификатор пользователя в Telegram")
     private Long userTelegramId;
 
     /**
      * Имя пользователя.
      */
+    @NotBlank(message = "Имя пользователя не должно быть пустым")
     @Schema(description = "Имя пользователя")
     private String username;
 
     /**
      * Предпочтительный пол пользователя.
      */
+    @Valid
     @Schema(description = "Информация о предпочтениях пользователя")
     private UserPreferenceRequest userPreference;
 
     /**
      * Подробная информация о пользователе.
      */
+    @Valid
     @Schema(description = "Подробная информация о пользователе")
     private UserInfoRequest userInfo;
 }
--- user-details-service/src/main/java/com/cupidmeet/userdetailsservice/user/domain/dto/UserInfoRequest.java
+++ user-details-service/src/main/java/com/cupidmeet/userdetailsservice/user/domain/dto/UserInfoRequest.java
@@ -3,6 +3,8 @@ package com.cupidmeet.userdetailsservice.user.domain.dto;
 import com.cupidmeet.userdetailsservice.user.domain.types.Gender;
 import com.cupidmeet.userdetailsservice.user.domain.types.PersonalityType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.Set;
@@ -17,42 +19,49 @@ public class UserInfoRequest {
     /**
      * Имя пользователя.
      */
+    @NotNull
     @Schema(description = "Имя пользователя")
     private String name;
 
     /**
      * Возраст пользователя.
      */
+    @NotNull(message = "Возраст пользователя не может быть пустым")
     @Schema(description = "Возраст пользователя")
     private Integer age;
 
     /**
      * Город проживания пользователя.
      */
+    @NotBlank(message = "Город проживания пользователя не может быть пустым")
     @Schema(description = "Город проживания пользователя")
     private String city;
 
     /**
      * Пол пользователя.
      */
+    @NotBlank(message = "Пол пользователя не может быть пустым")
     @Schema(description = "Пол пользователя")
     private Gender gender;
 
     /**
      * Тип личности пользователя.
      */
+    @NotBlank(message = "Тип личности пользователя не может быть пустым")
     @Schema(description = "Тип личности пользователя")
     private PersonalityType personalityType;
 
     /**
      * Краткая биография пользователя.
      */
+    @NotNull(message = "Тип личности пользователя не может быть пустым")
     @Schema(description = "Краткая биография пользователя")
     private String bio;
 
     /**
      * Вложенные файлы пользователя.
      */
+    @NotNull(message = "Вложенные файлы пользователя не могут быть пустым")
     @Schema(description = "Вложенные файлы пользователя")
     private Set<String> fileIds;
 }

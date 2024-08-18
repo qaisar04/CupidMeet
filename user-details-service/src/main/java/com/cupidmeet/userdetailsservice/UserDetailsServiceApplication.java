8ccc3f859e159aff6281694100057d8936b3eef1
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-08-18 19:01:44 +0300

    Небольшие изменения

--- user-details-service/src/main/java/com/cupidmeet/userdetailsservice/UserDetailsServiceApplication.java
+++ user-details-service/src/main/java/com/cupidmeet/userdetailsservice/UserDetailsServiceApplication.java
@@ -4,10 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableFeignClients
 @SpringBootApplication(scanBasePackages = "com.cupidmeet")
 @OpenAPIDefinition(info = @Info(title = "Сервис \"User Details\"", version = "1.0"))
 @SuppressWarnings({"checkstyle:UncommentedMain", "checkstyle:HideUtilityClassConstructor"})

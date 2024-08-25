8f10f950d48e6719d5cd17ea5cc97f228f975570
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-08-25 22:30:59 +0300

    Изменения связанные с minio

--- storage-service/src/main/java/kz/baltabayev/storageservice/domain/entity/BaseEntity.java
+++ storage-service/src/main/java/kz/baltabayev/storageservice/domain/entity/BaseEntity.java
@@ -12,11 +12,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-/**
- * Abstract class provides fields for classes-descendants to make database audit on timing of
- * creating, updating and deleting entity. This allows to differentiate between business logic and
- * audit
- */
 @Getter
 @Setter
 @ToString
@@ -25,23 +20,14 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-  /**
-   * The timestamp representing the creation time of the entity.
-   */
   @CreationTimestamp
   @Column(name = "created_at")
   private LocalDateTime createdAt;
 
-  /**
-   * The timestamp representing the last update time of the entity.
-   */
   @UpdateTimestamp
   @Column(name = "updated_at")
   private LocalDateTime updatedAt;
 
-  /**
-   * The timestamp representing the deletion time of the entity.
-   */
   @UpdateTimestamp
   @Column(name = "deleted_at")
   private LocalDateTime deletedAt;

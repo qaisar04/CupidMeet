28109757e72f8c80cc47b487ea8eff22b2f0bf01
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-10-15 06:23:54 +0300

    Дополнительная настройка безопастности

--- storage-service/src/main/java/kz/baltabayev/storageservice/StorageServiceApplication.java
+++ storage-service/src/main/java/kz/baltabayev/storageservice/StorageServiceApplication.java
@@ -1,7 +1,11 @@
 package kz.baltabayev.storageservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
@@ -11,6 +15,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Сервис \"Storage\"", version = "1.0"))
+@SecurityScheme(
+        name = "bearerToken",
+        type = SecuritySchemeType.OAUTH2,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        flows = @OAuthFlows(authorizationCode = @OAuthFlow(
+                authorizationUrl = "https://qaisar.online:8843/realms/cupid-meet-dev/protocol/openid-connect/auth",
+                tokenUrl = "https://qaisar.online:8843/realms/cupid-meet-dev/protocol/openid-connect/token"))
+)
 public class StorageServiceApplication {
 
     public static void main(String[] args) {
